e102a09507e7457ed689ee2ab2daf8e12e8f3723
Aaron Wheeler <aaronwheeler@aaron-wheelers-macbook-pro-15.local>
2008-02-27 14:01:09 +0900

    Changed routing path call to use MERB's routing #url method

--- config/router.rb
+++ config/router.rb
@@ -26,7 +26,7 @@ Merb::Router.prepare do |r|
 
   r.resources :blogs
 
-  r.match('/:year/:month/:path_title').to( :controller => 'blogs', :action => 'show' )
+  r.match('/:year/:month/:path_title').to( :controller => 'blogs', :action => 'show' ).name( :blog )
 
   # This is the default route for /:controller/:action/:id
   # This is fine for most cases.  If you're heavily using resource-based
