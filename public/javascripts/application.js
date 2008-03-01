d43251aa069c9fefc0c518a7e8793cbe5d7d1fe3
Aaron Wheeler <aaronwheeler@aaron-wheelers-macbook-pro-15.local>
2008-03-01 23:42:26 +0900

    Added support for displaying Flickr image

---
+++ public/javascripts/application.js
@@ -0,0 +1,12 @@
+// Finds the most recent photo in Flickr and embeds it in the page
+function load_flickr_photo() {
+  // var url = 'http://api.flickr.com/services/feeds/photos_public.gne?id=70203521@N00&amp;lang=en-us&amp;format=rss_200';
+  var url = '/flickr.rss'
+  $.get(url, {}, function(xml,stat) {
+    //photo = $(xml).find('entry link[@type="image/jpeg"]')[0].href;
+    regexp = /http:\/\/farm\d.static.flickr.com\/\d+\/\d+_[\da-f]*_m.jpg/
+    photo  = $(xml).find('entry content')[0].innerHTML.match(regexp)[0];
+    photo  = photo.replace('_m.jpg', '_t.jpg');
+    $('#photos + div').html('<img src="' + photo + '" />');
+  })
+}
