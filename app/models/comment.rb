c39e2e22626816a2403178897862f14b50b4b828
Aaron Wheeler <aaronwheeler@aaron-wheelers-macbook-pro-15.local>
2008-03-02 22:10:12 +0900

    Added (failing) tests for comments and save prevention due to comments being closed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,22 @@
 class Comment < DataMapper::Base
   include DataMapper::Reflection
 
-  property :body, :text
-  property :created_at, :datetime
-  property :author_name, :text
+  property :body,           :text
+  property :created_at,     :datetime
+  property :author_name,    :text
   property :author_website, :text
 
   belongs_to :blog
+
+  before_save :validate_open_comments
+
+  private
+    def validate_open_comments
+      if self.blog.comments_closed?
+        self.errors.add( :blog_id, 'Comments are closed for this blog' )
+        return false
+      else
+        return true
+      end
+    end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,7 +1,16 @@
 require File.join( File.dirname(__FILE__), "..", "spec_helper" )
 
-describe Comment do
+describe Comment, 'Callback methods' do
+  before(:each) do
+    @today     = Blog.new( :title => 'today',     :comments_expire_at => mock('datetime', :to_date => Date.today ) )
+    @tomorrow  = Blog.new( :title => 'tomorrow',  :comments_expire_at => mock('datetime', :to_date => Date.today + 1) )
+    @yesterday = Blog.new( :title => 'yesterday', :comments_expire_at => mock('datetime', :to_date => Date.today - 1) )
+  end
 
-  it "should have specs"
-
+  it 'should know when comments close' do           # comments_closed?
+#    lambda { Comment.count }.should be_different_by(0) { Comment.create( :blog => @yesterday) }
+    Comment.create( :blog => @yesterday).new_record?.should == true
+    Comment.create( :blog => @today).new_record?.should     == true
+    Comment.create( :blog => @tomorrow).new_record?.should  == false
+  end
 end
