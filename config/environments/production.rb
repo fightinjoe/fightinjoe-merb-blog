66c80b70126912d3d5aa6fd70d0a54c8877870f0
Aaron Wheeler <aaronwheeler@aaron-wheelers-macbook-pro-15.local>
2008-03-16 23:39:48 +0900

    Fixed model class reloading bug in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Merb.logger.info("Loaded PRODUCTION Environment...")
 Merb::Config.use { |c|
   c[:exception_details] = false
-  c[:reload_classes] = false
+  c[:reload_classes] = true
+  c[:reload_time] = 0.5
 }
